75a8b89e2209e574fe5920d6969dcdf900c67f87
leeyoulan <youlanlee3@gmail.com>
2017-11-07 21:05:36 +0900

    board_web

--- web/router/main/main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-
-router.get('/',function(req,res){
-    res.render('main/main.ejs',{'userId':req.user});
-  })
-
-module.exports = router;
