c198fea406e9fb665813ecc96c1d2b71bb772db0
leeyoulan <youlanlee3@gmail.com>
2017-11-09 03:46:17 +0900

    board_mobile

--- mobile/router/index.js
+++ mobile/router/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var path = require('path');
 
 var members = require('./members/members');
 var posts = require('./posts/posts');
