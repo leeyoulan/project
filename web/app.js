ef2c6fc5cb1d705ed08f1f86623ab5089d9ac047
leeyoulan <youlanlee3@gmail.com>
2017-11-10 07:08:21 +0900

    chat

--- web/app.js
+++ web/app.js
@@ -20,6 +20,8 @@ var cors = require('cors');
 
 // 익스프레스 객체 생성
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
@@ -43,7 +45,24 @@ app.use(flash());
 
 var router = require('./router/index');
 
-app.listen(50005,function(){
+var count=1;
+io.on('connection', function(socket){ //3
+  console.log('user connected: ', socket.id);  //3-1
+  var name = "사용자" + count++;                 //3-1
+  io.to(socket.id).emit('change name',name);   //3-1
+
+  socket.on('disconnect', function(){ //3-2
+    console.log('user disconnected: ', socket.id);
+  });
+
+  socket.on('send message', function(name,text){ //3-3
+    var msg = name + ' : ' + text;
+    console.log(msg);
+    io.emit('receive message', msg);
+  });
+});
+
+http.listen(50005,function(){
   console.log('start! port 50005');
 });
 
--- web/router/main/main.js
+++ web/router/main/main.js
@@ -6,4 +6,5 @@ router.get('/',function(req,res){
     res.render('main/main.ejs',{'userId':req.user});
   })
 
+
 module.exports = router;
